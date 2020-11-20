f3c17d58323c2081f71cddbc6f5b762dd5db1fb7
mjmccoach <mmccoach88@gmail.com>
2020-11-20 15:14:52 +0000

    Initial commit

---
+++ src/main/java/Commit.java
@@ -0,0 +1,11 @@
+public class Commit {
+    private String description;
+    private int uniqueId;
+
+    public Commit(String description, int uniqueId){
+        this.description = description;
+        this.uniqueId = uniqueId;
+    }
+
+    public String getDescription()
+}
---
+++ src/test/java/CommitTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class CommitTest {
+   Commit commit1;
+
+   @Before
+   public void before() {
+      commit1 = new Commit("Initial Commit", 1);
+   }
+    
+
+
+}
